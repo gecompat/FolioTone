aeacd86725c1ba1a10be591bc4fd8c30fb7f49ab
Gerhard P <github@gecomp.at>
2026-08-09 00:16:15 +0200

    feat: export generic ToolProvider runtime

--- src/foliotone/tooling/__init__.py
+++ src/foliotone/tooling/__init__.py
@@ -2,5 +2,24 @@
 
 from foliotone.tooling.artifacts import ToolArtifact
 from foliotone.tooling.contracts import ToolExecution, ToolProviderDescriptor, ToolResult
+from foliotone.tooling.runtime import (
+    ContainerCommand,
+    LocalCommand,
+    ReadOnlyMount,
+    ToolRunOutcome,
+    ToolRuntime,
+    build_docker_argv,
+)
 
-__all__ = ["ToolArtifact", "ToolExecution", "ToolProviderDescriptor", "ToolResult"]
+__all__ = [
+    "ContainerCommand",
+    "LocalCommand",
+    "ReadOnlyMount",
+    "ToolArtifact",
+    "ToolExecution",
+    "ToolProviderDescriptor",
+    "ToolResult",
+    "ToolRunOutcome",
+    "ToolRuntime",
+    "build_docker_argv",
+]
