1a8f8c1e1a1c9cd7c18b5655084b3f19f04e2b46
Gerhard P <github@gecomp.at>
2026-08-08 23:33:34 +0200

    feat: export ToolProvider contracts

--- src/foliotone/tooling/__init__.py
+++ src/foliotone/tooling/__init__.py
@@ -1,1 +1,5 @@
 """External specialist tool orchestration behind adapter-neutral contracts."""
+
+from foliotone.tooling.contracts import ToolExecution, ToolProviderDescriptor, ToolResult
+
+__all__ = ["ToolExecution", "ToolProviderDescriptor", "ToolResult"]
