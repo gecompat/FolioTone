37a121e8a48902ffc28483ed88305687496f4268
Gerhard P <github@gecomp.at>
2026-08-19 08:28:41 +0200

    feat: serialize consolidation plans canonically

--- src/foliotone/consolidation/__init__.py
+++ src/foliotone/consolidation/__init__.py
@@ -1,3 +1,20 @@
 """Public immutable contracts for non-executable consolidation planning."""
 
+from foliotone.consolidation import contracts as _contracts
 from foliotone.consolidation.contracts import *  # noqa: F403
+from foliotone.consolidation.serialization import (
+    canonical_consolidation_plan_payload,
+    canonical_plan_bytes,
+    compute_consolidation_plan_content_hash,
+    consolidation_plan_content_hash,
+    serialize_consolidation_plan,
+)
+
+__all__ = [
+    *_contracts.__all__,
+    "canonical_consolidation_plan_payload",
+    "canonical_plan_bytes",
+    "compute_consolidation_plan_content_hash",
+    "consolidation_plan_content_hash",
+    "serialize_consolidation_plan",
+]
